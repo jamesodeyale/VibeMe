7d4c5c9163f1c5d5619f021c8b66f7fea4187202
James Odeyale <jamesodeyale01@gmail.com>
2022-06-17 21:00:07 -0400

    initializing new project

---
+++ client/starter.md
@@ -0,0 +1,1 @@
+This is a client for VibeMe which would be written in React Native
---
+++ server/src/main/java/me/vibeserver/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package me.vibeserver.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
